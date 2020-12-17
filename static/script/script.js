e2d453cac1de333ef110451837a69575564c4102
Mostakin Ahmed <rohitkhan4141@gmail.com>
2020-12-17 20:07:25 +0600

    scroll down in chrome

--- static/script/script.js
+++ static/script/script.js
@@ -122,3 +122,10 @@ Array.from(document.querySelectorAll(".ImgThumbnail")).forEach((item) => {
 document.querySelector(".close").addEventListener("click", () => {
   modalEle.style.display = "none";
 });
+
+function scrollToBottom() {
+  window.scrollTo(0, document.body.scrollHeight);
+}
+if (navigator.appVersion.indexOf("Chrome/") != -1) {
+  scrollToBottom();
+}
