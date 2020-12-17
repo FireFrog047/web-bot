fbfa386df2d861d9a49f601dc43281d0b7ad9105
Monjurul <monjurul.47@gmail.com>
2020-12-17 17:53:09 +0600

    home route added

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.static_folder = 'static'
 
+@app.route("/")
+def home(userName):
+    if f"/{userName}" in response.userDetails:
+        return render_template("index.html", 
+            botNameAW="Rihana",
+            botProfielPictureAW="https://i.pinimg.com/originals/f7/85/10/f78510d0487088afb50abd9eedd477a1.jpg",
+
+            )
+    else:
+        return render_template("404.html")
+
 @app.route("/<userName>")
 def home(userName):
     if f"/{userName}" in response.userDetails:
